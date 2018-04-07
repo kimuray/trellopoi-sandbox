5d8bdb4b05ee8c232763a3ff1f3cc2800eddd6b8
Yoshihiro Kimura <kimura.yoshihiro@tsunolab.net>
2018-04-08 00:21:40 +0900

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,46 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <draggable :options="{ group: 'tasks' }" element="ul" v-for="list in tasks">
+      <li v-for="item in list">{{ item }}</li>
+    </draggable>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import draggable from 'vuedraggable';
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    draggable,
+  },
+  data() {
+    return {
+      tasks: [
+        ['task1-1', 'task1-2', 'task1-3', 'task1-4'],
+        ['task2-1', 'task2-2', 'task2-3', 'task2-4'],
+        ['task3-1', 'task3-2', 'task3-3', 'task3-4']
+      ]
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  display: flex;
+  justify-content: space-around;
+}
+ul {
+  list-style: none;
+  padding: 20px;
+  margin: 20px;
+  border: 1px solid #222;
+}
+li {
+  cursor: pointer;
+  padding: 10px;
+  border: 1px solid #222;
+  margin-bottom: 10px;
 }
 </style>
